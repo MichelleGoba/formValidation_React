ca04f712730106e4aa1575101d047051a3d6d65d
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-08-24 15:21:57 +0200

    handleChange function

--- formValidation/src/components/contact/Contact.jsx
+++ formValidation/src/components/contact/Contact.jsx
@@ -4,19 +4,28 @@ import React, { useState } from 'react';
 function Contact() {
     const initialValue = {name:"", email:'', subject:"", message:'',};
     const [formValues, setFrmValues] = useState(initialValue);
+
+    const handleChange = (e) => {
+
+        const {name, value} = e.target;
+        setFrmValues({...formValues, [name]:value});
+        console.log(formValues);
+    };
+
   return (
-    <div className='contact-container'>   
+    <div className='contact-container'> 
+    <form onSubmit={handleSubmit}>
         <div className='contact-form'>
-          <div className='row'>
+          
             <div className='group1'>
               <div className='input'>
-              <input type='text' placeholder='Name' className='form-text' value={formValues.name}></input>
+              <input type='text' placeholder='Name' className='form-text' value={formValues.name} onChange={handleChange}></input>
               </div>
             </div>
             
             <div className='group1'>
               <div className='input'>
-              <input type='text' placeholder='Email' className='form-text' value={formValues.email}></input>
+              <input type='text' placeholder='Email' className='form-text' value={formValues.email} onChange={handleChange}></input>
               </div>
             </div>
           </div>
@@ -24,7 +33,7 @@ function Contact() {
           <div className='row'>
           <div className='group2'>
               <div className='input'>
-              <input type='text' placeholder='Subject' className='form-text' value={formValues.subject}></input>
+              <input type='text' placeholder='Subject' className='form-text' value={formValues.subject} onChange={handleChange}></input>
               </div>
             </div>
           </div>
@@ -32,7 +41,7 @@ function Contact() {
           <div className='row'>
           <div className='group2'>
               <div className='input'>
-              <textarea name="" id="message" className='form-text'  placeholder="Message" value={formValues.message}></textarea>
+              <textarea name="" id="message" className='form-text'  placeholder="Message" value={formValues.message} onChange={handleChange}></textarea>
               </div>
             </div>
           </div>
@@ -43,12 +52,12 @@ function Contact() {
             </div>
           </div>
 
-
+          </form>  
         </div>
 
       
       
-      </div>
+      
 
   )
 }
